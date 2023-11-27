798bcd0d518e15aed941f20197423f6b0fed769f
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-11-27 10:21:20 +0100

    Initial release

--- action.ps1
+++ action.ps1
@@ -1,64 +1,61 @@
-# HelloID-Task-SA-Target-<System>-<Description of action>
+# HelloID-Task-SA-Target-HelloID-GroupGrantMembership
 ###########################################################
 # Form mapping
 $formObject = @{
-    userName           = $form.userName
-    firstName          = $form.firstName
-    lastName           = $form.lastName
-    contactEmail       = $form.contactEmail
-    isEnabled          = [bool]$form.isEnabled
-    password           = $form.password
-    mustChangePassword = [bool]$form.mustChangePassword
+    groupToAddGUID = $form.groupToAddGUID
+    groupToAddName = $form.groupToAddName
+    groupGUID      = $form.groupGUID
+    groupName      = $form.groupName
 }
 
 try {
-    Write-Information "Executing <System> action: [CreateAccount] for: [$($formObject.userName)]"
+    Write-Information "Executing HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)]"
     Write-Verbose "Creating authorization headers"
-    # Create authorization headers with <System> API key
+    # Create authorization headers with HelloID API key
     $pair = "${portalApiKey}:${portalApiSecret}"
     $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
     $base64 = [System.Convert]::ToBase64String($bytes)
     $key = "Basic $base64"
     $headers = @{"authorization" = $Key }
 
-    Write-Verbose "Creating <System>Account for: [$($formObject.userName)]"
-    $splatCreateUserParams = @{
-        Uri         = "$($portalBaseUrl)/api/v1/users"
+    Write-Verbose "Granting Membership to HelloIDGroup: [$($formObject.groupName)] for HelloIDGroup: [$($formObject.groupToAddName)]"
+    $splatAddGroupParams = @{
+        Uri         = "$($portalBaseUrl)/api/v1/groups/$($formObject.groupGUID)/membergroups"
         Method      = "POST"
-        Body        = ([System.Text.Encoding]::UTF8.GetBytes(($formObject | ConvertTo-Json -Depth 10)))
+        Body        = ([System.Text.Encoding]::UTF8.GetBytes((@{ "groupGuid" = $formObject.groupToAddGUID } | ConvertTo-Json -Depth 10)))
         Verbose     = $false
         Headers     = $headers
         ContentType = "application/json"
     }
-    $response = Invoke-RestMethod @splatCreateUserParams
+    $response = Invoke-RestMethod @splatAddGroupParams
 
     $auditLog = @{
-        Action            = "CreateAccount"
-        System            = "<System>"
-        TargetIdentifier  = [String]$response.userGUID
-        TargetDisplayName = [String]$response.userName
-        Message           = "<System> action: [CreateAccount] for: [$($formObject.userName)] executed successfully"
+        Action            = "GrantMembership"
+        System            = "HelloID"
+        TargetIdentifier  = [String]$formObject.groupToAddGUID
+        TargetDisplayName = [String]$formObject.groupToAddName
+        Message           = "HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)] executed successfully"
         IsError           = $false
     }
     Write-Information -Tags "Audit" -MessageData $auditLog
 
-    Write-Information "<System> action: [CreateAccount] for: [$($formObject.userName)] executed successfully"
+    Write-Information "HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)] executed successfully"
 }
 catch {
     $ex = $_
     $auditLog = @{
-        Action            = "CreateAccount"
-        System            = "<System>"
-        TargetIdentifier  = ""
-        TargetDisplayName = [String]$formObject.userName
-        Message           = "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.Exception.Message)"
+        Action            = "GrantMembership"
+        System            = "HelloID"
+        TargetIdentifier  = [String]$formObject.groupToAddGUID
+        TargetDisplayName = [String]$formObject.groupToAddName
+        Message           = "Could not execute HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)], error: $($ex.Exception.Message)"
         IsError           = $true
     }
     if ($($ex.Exception.GetType().FullName -eq "Microsoft.PowerShell.Commands.HttpResponseException")) {
-        $auditLog.Message = "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)]"
-        Write-Error "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.ErrorDetails)"
+        $auditLog.Message = "Could not execute HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)]"
+        Write-Error "Could not execute HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)], error: $($ex.ErrorDetails)"
     }
     Write-Information -Tags "Audit" -MessageData $auditLog
-    Write-Error "Could not execute <System> action: [CreateAccount] for: [$($formObject.userName)], error: $($ex.Exception.Message)"
+    Write-Error "Could not execute HelloID action: [GrantMembership] to Group: [$($formObject.groupName)] for Group: [$($formObject.groupToAddName)], error: $($ex.Exception.Message)"
 }
 ###########################################################
